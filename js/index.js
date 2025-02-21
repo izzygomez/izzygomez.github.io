880afa805030a8fd5cb9d7f1880567cb97ac746e
Izzy Gomez <izzy@izzygomez.com>
2025-02-21 14:31:48 -0500

    TEMP attempt to fix initial page load issues when deployed

--- js/index.js
+++ js/index.js
@@ -16,29 +16,36 @@ document.addEventListener("DOMContentLoaded", function () {
 // Currently defaults to: DARK.
 // If this is changed, also update inline script in _includes/head.html
 document.addEventListener("DOMContentLoaded", function () {
+  // Apply transitions only after initial load
+  document.body.classList.add("loaded");
+  const allLinks = document.querySelectorAll("a");
+  allLinks.forEach((link) => {
+    link.classList.add("loaded");
+  });
+
   const colorOptions = document.querySelectorAll(".color-option");
 
   function setTheme(mode) {
-    document.documentElement.setAttribute("data-theme", mode);
-    localStorage.setItem("theme", mode);
+    document.documentElement.setAttribute("color-theme", mode);
+    localStorage.setItem("color-theme", mode);
 
     // Update the selected state in UI
     colorOptions.forEach((option) => {
       option.classList.toggle(
         "selected",
-        option.getAttribute("theme-option") === mode,
+        option.getAttribute("color-theme-option") === mode,
       );
     });
   }
 
   // Load stored theme or fallback to dark
-  const savedTheme = localStorage.getItem("theme") || "dark";
+  const savedTheme = localStorage.getItem("color-theme") || "dark";
   setTheme(savedTheme);
 
   // Set event listeners for theme selection
   colorOptions.forEach((option) => {
     option.addEventListener("click", function () {
-      const selectedTheme = this.getAttribute("theme-option");
+      const selectedTheme = this.getAttribute("color-theme-option");
       setTheme(selectedTheme);
     });
   });
